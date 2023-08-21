2b351c8bb27fd3f21657f1aa019e1c905c50a324
duongb1910356 <duongb1910356@student.ctu.edu.vn>
2023-08-21 07:49:28 +0700

    Feat: init project fitivation

---
+++ src/main/java/com/fitivation_v3/FitivationV3Application.java
@@ -0,0 +1,13 @@
+package com.fitivation_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitivationV3Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FitivationV3Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/fitivation_v3/FitivationV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitivation_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitivationV3ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
